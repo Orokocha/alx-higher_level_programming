7fd0e7fe4a8dc7bd2c9aab94f07c8926c080c1b6
Orokocha <orokocharles01@gmail.com>
2024-01-10 02:15:49 -0800

    new

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,19 @@
-#!/usr/bin/python3
 
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [row[:] for row in matrix]
-    for idx, row in enumerate(new_matrix):
-        for idx2, col in enumerate(new_matrix):
-            new_matrix[idx][idx2] = row[idx2] ** 2
-    return new_matrix 
+    squared = []
+    for line in matrix:
+        squared.append([c**2 for c in line])
+    return squared
+
+after creating each files, cat 0-main.py and copy the code they gave as an example by using vi 0-mmain
+
+then chmod u+x
+
+
+vi 1-search_replace.py
+
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    return [replace if search == n else n for n in my_list]
+
