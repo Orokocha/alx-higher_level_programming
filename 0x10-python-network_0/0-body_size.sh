f9984ac6d7de1999d0920d05a349ae45a28d1c29
Orokocha <orokocharles01@gmail.com>
2024-04-25 12:36:00 -0700

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
-
-# Check if a URL is provided as an argument
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get URL from command line argument
-URL=$1
-
-# Send request to the URL and save the response body to a temporary file
-RESPONSE=$(curl -s -o /tmp/response_body "$URL")
-
-# Get the size of the response body in bytes
-SIZE=$(wc -c < /tmp/response_body)
-
-# Display the size of the response body
-echo "Size of the response body: $SIZE bytes"
-
-# Clean up temporary file
-rm -f /tmp/response_body
-
+# cURL get body size
+curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f 2
